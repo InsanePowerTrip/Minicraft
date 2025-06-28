34616326520d39008756d3551f139811463c97cd
Power Trip <96303911+InsanePowerTrip@users.noreply.github.com>
2025-06-28 13:10:08 +0400

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 @echo off
-gcc main.c -I./glfw -L./glfw/lib -l:libglfw3.a -lopengl32 -lglu32 -lgdi32 -luser32 -lkernel32 -lwinmm -lwinpthread -mwindows -o main.exe
+gcc main.c -I./glfw -L./glfw/lib -l:libglfw3.a -lopengl32 -lglu32 -lgdi32 -luser32 -lkernel32 -lwinmm -lpthread -mwindows -static -static-libgcc -static-libstdc++ -o main.exe
 if %errorlevel% neq 0 pause
